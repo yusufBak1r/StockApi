bba84e8428851c93bcc4776c530dabd680ae6d2d
yusuf <yusuf.bak1r3637@gmail.com>
2024-07-16 02:03:02 +0300

    stock api was made

---
+++ Data/ApplicationDBContext.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.OpenApi.Models;
+namespace api.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+          public ApplicationDbContext(DbContextOptions <ApplicationDbContext> options)
+           :base(options)
+        {
+
+        }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Model yapılandırmaları burada
+        }
+     
+        public DbSet<Stock> Stocks { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+    }
+}
